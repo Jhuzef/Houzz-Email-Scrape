351b85e65d88806beae150b0fece527aee88015e
Sybl <josephgonzales034@gmail.com>
2018-05-21 23:29:41 -0400

    Added E-mail Scraping functionality

--- Houzz_Email_Scrape.py
+++ Houzz_Email_Scrape.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import requests
 import urllib2
 from bs4 import BeautifulSoup
-import csv 
+import csv
 import sys
 import time
 import random
+from webscraping import download
 
 
-# Takes one argument:
+# Takes two arguments:
 # 1. City and State in the "[city]--[State Initial]" format
 # 2. The amount of records available in a specific search
 
@@ -23,7 +24,16 @@ def WriteDictToCSV(csv_columns,dict_data):
 			print row
 			writer.writerow(row)
 
-csv_columns =[ 'Name', 'Website']
+
+def EmailScrape(email):
+	D = download.Download()
+	emails = D.get_emails("{}".format(email), max_depth=1, max_urls=None, max_emails=25)
+	return emails
+
+csv_columns =[ 'Name', 'Website', 'Email 1', 'Email 2', 'Email 3', 'Email 4', 'Email 5',
+'Email 6', 'Email 7', 'Email 8', 'Email 9', 'Email 10', 'Email 11', 'Email 12', 'Email 13',
+'Email 14', 'Email 15', 'Email 16', 'Email 17', 'Email 18', 'Email 19', 'Email 20', 'Email 21',
+'Email 22', 'Email 23', 'Email 24', 'Email 25']
 
 
 for k in range(0, int(sys.argv[2]), 15):
@@ -53,7 +63,9 @@ for k in range(0, int(sys.argv[2]), 15):
 			if web is not None:
 				website = web["href"]
 				output["Website"] = website
-
+				emails = EmailScrape(website)
+				for x in range(0, len(emails)):
+					output["Email {}".format(x+1)] = emails[x]
 			data.append(output)
 
 			print output
@@ -62,13 +74,3 @@ for k in range(0, int(sys.argv[2]), 15):
 
 	#Writes to "houzz.csv every 15 successful entries"
 	WriteDictToCSV(csv_columns,data)
-
-
-
-
-
-
-
-
-
-
